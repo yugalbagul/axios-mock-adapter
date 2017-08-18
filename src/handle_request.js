9818670d5397394a03487ad737911312bb690f02
Yugal <yugalbagul@Yugals-MacBook-Air.local>
2017-08-18 19:10:54 +0530

    Added statusText in response

--- src/handle_request.js
+++ src/handle_request.js
@@ -7,7 +7,8 @@ function makeResponse(result, config) {
     status: result[0],
     data: utils.isSimpleObject(result[1]) ? JSON.parse(JSON.stringify(result[1])) : result[1],
     headers: result[2],
-    config: config
+    config: config,
+    statusText: result[3]
   };
 }
 
